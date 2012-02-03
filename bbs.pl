11394cb22a9bd043bfa6942945cd0f22c4404ff4
nqounet <nqou.net@gmail.com>
2012-02-03 21:11:37 +0900

    use Perl::Tidy;

--- bbs.pl
+++ bbs.pl
@@ -1,23 +1,23 @@
 #!/usr/bin/env perl
 use utf8;
+
 #ｕｔｆ８
 use Mojolicious::Lite;
 use Mojo::ByteStream qw(b);
 
 use Path::Class qw(file);
 
-app->secret( b( file(__FILE__)->absolute )->md5_sum );
+app->secret(b(file(__FILE__)->absolute)->md5_sum);
 
 helper data_path => sub {
-  return file(__FILE__)->absolute . '.dat'
+  return file(__FILE__)->absolute . '.dat';
 };
 
 helper get_messages => sub {
   my $data_path = app->data_path;
-  my $file = file( $data_path );
+  my $file      = file($data_path);
   $file->touch;
-  return split /\n/, b( $file->slurp )
-    ->decode( app->renderer->encoding );
+  return split /\n/, b($file->slurp)->decode(app->renderer->encoding);
 };
 
 get '/' => sub {
@@ -26,22 +26,19 @@ get '/' => sub {
 
 get '/index' => sub {
   my $self = shift;
-  $self->render(
-    messages => [ $self->get_messages ],
-  );
+  $self->render(messages => [$self->get_messages]);
 };
 
 post '/index' => sub {
-  my $self = shift;
+  my $self     = shift;
   my @messages = $self->get_messages;
-  my $msg = $self->param("msg");
+  my $msg      = $self->param("msg");
   unshift @messages, $msg;
   my $file_path = $self->data_path;
-  my $new_file = Mojo::Asset::File->new;
+  my $new_file  = Mojo::Asset::File->new;
   $new_file->add_chunk(
-    b(join "\n", @messages)
-      ->encode( app->renderer->encoding )
-  )->move_to( $file_path );
+    b(join "\n", @messages)->encode(app->renderer->encoding))
+    ->move_to($file_path);
   $self->redirect_to('index');
 };
 
